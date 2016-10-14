f9c1963041aa65a13ac78e09fb281c635ff50445
dshor97 <dayton.shor@gmail.com>
2016-10-13 22:45:54 -0400

    Add files via upload

---
+++ CardsInDeck.java
@@ -0,0 +1,5 @@
+package hw1;
+
+public enum CardsInDeck {
+	Two,Three, Four,Five,Six,Seven,Eight,Nine,Ten,Jack,Queen,King,Ace;
+}
---
+++ SuitOfCards.java
@@ -0,0 +1,5 @@
+package hw1;
+
+public enum SuitOfCards {
+	Clubs,Diamonds,Hearts,Spades;
+}
